419b4b5b3d8a24a21cf9091ddc022c33189f4226
andy_t <andrei.tsylko@rk-software.net>
2024-01-25 13:00:47 +0100

    feat: implement hash calculating logic

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import * as crypto from "crypto";
+import {HASH_ALGORITHM, HASH_ENCODING, PATH_TO_FILE} from "./hash.constant.js";
+import {getPathToFile} from "./util.js";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash(HASH_ALGORITHM);
+    const fileToCalculateHash = fs.createReadStream(getPathToFile(PATH_TO_FILE));
+    fileToCalculateHash.on('data', (data) => {
+        const calculatedHash = hash.update(data).digest(HASH_ENCODING)
+        console.log(calculatedHash) ;
+    });
 };
 
 await calculateHash();
